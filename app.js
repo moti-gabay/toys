94936f20ec324bccda4f07b6dbaabace012d530c
Moti Gabay <motigabay18@gmail.com>
2023-06-04 11:25:11 +0300

    ok

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const http = require("http");
 const path = require("path");
+const cors = require("cors");
 
 const {routesInit} = require("./routes/configRoutes")
 require("./db/mongoConnect");
@@ -8,6 +9,7 @@ require("./db/mongoConnect");
 const app = express();
 
 app.use(express.json());
+app.use(cors())
 
 app.use(express.static(path.join(__dirname,"public")));
 
